62a33a0f40f9621cf264fbf9cd1aecdaaedcca4f
enrico-neri-zerynth <e.neri@zerynth.com>
2022-02-01 10:29:30 +0100

    frequency job firmware

---
+++ README.md
@@ -0,0 +1,35 @@
+#  Demo firmware - Frequency job
+
+This firmware is a simple demo firmware that publishes a random value every 10 seconds.
+It is possible to change the data publishing frequency sending a job to the device, called **set_frequency**.
+This job expects an int argument representing the new data publishing interval, in seconds.
+
+* Job name: **set_frequency**
+* Job payload: **{ "frequency": x}**
+
+Where **x** must be an integer between 1 and 300 (5 minutes) seconds. If you send the job with a invalid value, 
+the device will answer with an error message.
+
+You can send the job to the device using the Zerynth Cloud Web Interface (https://cloud.zerynth.com) or via HTTP request.
+
+
+### Send job request 
+* Method: *POST*
+* Endpoint: https://api.zdm.zerynth.com/v3/devices/{your_device_id}/jobs/set_frequency
+* Request body: 
+  ```json
+  {
+    "value": {
+      "frequency": 15
+    }
+  }
+  ```
+* Auth: the request's header must contain the following key-value pair
+  ```X-API-KEY: "{Zerynth Cloud API Key}"``` [you can create a new API key from the Settings section inside the workspace page of the Zerynth Cloud]
+
+
+### Get job status
+* Method: *GET*
+* Endpoint: https://api.zdm.zerynth.com/v3/devices/{your_device_id}/jobs/set_frequency
+* Auth: the request's header must contain the following key-value pair
+  ```X-API-KEY: "{Zerynth Cloud API Key}"```
--- main.py
+++ main.py
@@ -1,96 +1,56 @@
-###############################################################################
-# Hello Davra!
-###############################################################################
-
-# First, import the board module from the bsp (board support package).
-# The bsp loads board specific names, variables and settings so that
-# this project can be run on different Zerynth hardware
-# without changing a line of code.
 from bsp import board
-
-# The Zerynth Device Manager is the entrypoint for the zCloud.
-# Let's connect and send data to the ZDM with a simple example.
-# Before exeuting this code, please associate the device with your
-# ZDM account by selecting "ZDM target" in VSCode Control Panel.
-
 # Let's import the zdm module
 from zdm import zdm
 # We also need wifi or ethernet
 from networking import wifi
-# the DHT11 driver
-from components.dht11 import dht11
-# and some control over the button
-import gpio
 
+# Set the ssid and password of your wifi network
+ssid = "WiFi SSID"
+passwd = "WiFi Password"
 
-# how fast we produce messages in milliseconds
-publish_frequency = 5000
-# DHT11 data pin
-dhtpin = D18
-# and how many time the button has bee pressed
-btnpress = 0
+# Data publish interval
+frequency = 10000
 
-def on_press(pin, value):
-    global publish_frequency, btnpress
-    if publish_frequency<=3000:
-        publish_frequency = 10000
-    else:
-        publish_frequency= publish_frequency-1000
-    print("Now publishing every",publish_frequency,"milliseconds")
-    btnpress = btnpress+1
+def data_frequency(agent, args):
+    global frequency
+    print("Job request received!",args)
+    if not "frequency" in args:
+        return {"msg": "Invalid argument for frequency job: missing 'frequency' arg"}
 
-# configure button
-gpio.on_rise(D0, on_press, pull=INPUT_PULLUP)
+    f = args["frequency"]
 
+    if type(f)> PINTEGER:
+        message = "Invalid argument for frequency job: 'frequency' arg must be an integer. Received: " + str(type(f))
+        return {"msg": message}
 
-##################################################
-# Set the ssid and password of your wifi network!
-##################################################
-ssid = "Zerynth3"
-passwd = "zerynthwifi"
+    if f < 1 or f > 300: 
+        return {"msg": "Invalid argument for frequency job: 'frequency' arg must be between 1 and 300 seconds"}
 
+    frequency = f * 1000
+    return {"msg": "frequency set to %d seconds" % f }
+
+   
 while True:
     try:
         # Let's connect to the wifi
         print("configuring wifi...")
-        wifi.configure(
-            ssid=ssid,
-            password=passwd)
+        wifi.configure(ssid=ssid, password=passwd)
         print("connecting to wifi...")
         wifi.start()
         print("connected!",wifi.info())
 
-        # the Agent class implements all the logic for interacting with the ZDM
-        agent = zdm.Agent()
+        # the Agent class implements all the logic to talk with the ZDM
+        # it also accepts a dictionary of functions to be called as jobs
+        agent = zdm.Agent(jobs={"set_frequency":data_frequency})
         # just start it
         agent.start()
 
-        hum = None
-        temp = None
-                
         while True:
-            # use the agent to publish values to the ZDM
-            # Just open the device page from VSCode and check that data is incoming
-            try:
-                # DHT11 can fail reading 
-                hum, temp = dht11.read(dhtpin)
-            except Exception as e:
-                print("DHT11 failed to read!",e)
-
-                
-
-            agent.publish({
-                "hum":hum, 
-                "temp":temp, 
-                "freq":publish_frequency,
-                "btn":btnpress}, "davra")
-            
-            sleep(publish_frequency)
-            # The agent automatically handles connections and reconnections
-            print("ZDM is online:    ",agent.online())
-            # And provides info on the current firmware version
-            print("Firmware version: ",agent.firmware())
-            
+            v = random(0,100)
+            agent.publish(payload={"value":v}, tag="data")
+            print("Published",v)
+            sleep(frequency)
+           
         wifi.stop()
         print("disconnected from wifi")
     except WifiBadPassword:
@@ -101,7 +61,3 @@ while True:
         print("Generic Wifi Exception")
     except Exception as e:
         raise e
-
-    sleep(3000)
-
-
